4037f482a4fa4656975ac41c295f22e6b83fe1ec
Yevhen Hrytsai <yevgen.grytsay@gmail.com>
2024-07-20 10:02:20 +0300

    add deprecation comments to main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "helm_release" "fluent" {
   chart = "https://github.com/fluent/helm-charts/releases/download/fluent-bit-0.46.5/fluent-bit-0.46.5.tgz"
 
   reset_values  = true
-  recreate_pods = true
+  recreate_pods = true  # DEPRECATED in Helm 3 https://helm.sh/docs/intro/using_helm/#helpful-options-for-installupgraderollback
 
   values = [
     file("${path.module}/otel/fluent-bit/helm-values.yaml")
@@ -22,7 +22,7 @@ resource "helm_release" "opentelemetry_collector" {
   chart = "https://github.com/open-telemetry/opentelemetry-helm-charts/releases/download/opentelemetry-collector-0.90.1/opentelemetry-collector-0.90.1.tgz"
 
   reset_values  = true
-  recreate_pods = true
+  recreate_pods = true  # DEPRECATED in Helm 3 https://helm.sh/docs/intro/using_helm/#helpful-options-for-installupgraderollback
 
   values = [
     file("${path.module}/otel/collector/helm-values.yaml")
@@ -37,7 +37,7 @@ resource "helm_release" "loki" {
   version    = "6.5.2"
 
   reset_values  = true
-  recreate_pods = true
+  recreate_pods = true  # DEPRECATED in Helm 3 https://helm.sh/docs/intro/using_helm/#helpful-options-for-installupgraderollback
 
   values = [
     file("${path.module}/otel/loki/helm-values.yaml")
@@ -52,7 +52,7 @@ resource "helm_release" "grafana" {
   version    = "7.3.11"
 
   reset_values  = true
-  recreate_pods = true
+  recreate_pods = true  # DEPRECATED in Helm 3 https://helm.sh/docs/intro/using_helm/#helpful-options-for-installupgraderollback
 
   values = [
     file("${path.module}/otel/grafana/helm-values.yaml")
@@ -67,7 +67,7 @@ resource "helm_release" "prometheus" {
   version    = "25.21.0"
 
   reset_values  = true
-  recreate_pods = true
+  recreate_pods = true  # DEPRECATED in Helm 3 https://helm.sh/docs/intro/using_helm/#helpful-options-for-installupgraderollback
 
   values = [
     file("${path.module}/otel/prometheus/helm-values.yaml")
